18c840ce387e5f4f781573a07443ddbbcd200967
lucasviamaquinas <79279663+lucasviamaquinas@users.noreply.github.com>
2021-11-18 16:10:49 -0300

    A procura na lista está funcionando

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, FlatList, StatusBar } from 'react-native';
 
 export default function App() {
+
+  const [search, setSearch] = React.useState("");
+  const [data, setData] = React.useState([
+    {
+      id: 1,
+      name: "Amazon Prime Video"
+    },
+    {
+      id: 2,
+      name: "Netflix"
+    },
+    {
+      id: 3,
+      name: "HBO Max"
+    },
+    {
+      id: 4,
+      name: "Paramount+"
+    },
+  ]);
+  const [filteredData, setFilteredData] = React.useState([]);
+
+  const handleSearch = (searchText) => {
+    setSearch(searchText);
+
+    let filteredData = data.filter(item => {
+      return item.name.includes(searchText);
+    });
+
+    setFilteredData(filteredData);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput placeholder="Typing..." onChangeText={searchText => handleSearch(searchText)}/>
+
+      <Text>{search}</Text>
+
+      <FlatList
+        data={filteredData && filteredData.length > 0 ? filteredData : data }
+        renderItem={({item}) =>
+          <Text>{item.name}</Text>
+        }
+        keyExtractor={(item) => item.id.toString()}
+        />
+
+      <StatusBar barStyle="dark-content" backgroundColor="#E9E9E9" />
+    </SafeAreaView>
   );
 }
 
